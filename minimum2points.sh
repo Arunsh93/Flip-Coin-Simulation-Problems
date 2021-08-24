2f57f40913d7b9285e96c23b742be4efde5f694b
Arunsh93 <Arunhosur93@gmail.com>
2021-08-24 18:46:58 +0530

    Minimum 2 Points if tai

---
+++ minimum2points.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+Heads=1
+Tails=0
+
+read -p "Enter the Number of times U Want to Flip: " n
+
+for((i=0;i<=n;i++))
+do
+   randNumber=$((RANDOM%2))
+   if [ $randNumber -eq $Heads ]
+   then
+      echo "HEADS"
+         N=$((++HEADS))
+
+   else
+      echo "TAILS"
+         M=$((++TAILS))
+   fi
+
+   if [[ $N -eq 21 ]]
+   then
+      echo "Head Appears 21 Times and WON"
+      echo "Heads Won by: "$(($N-$M))
+      break
+   elif [[ $M -eq 21 ]]
+   then
+      echo "Tails Appears 21 Times and WON"
+      echo "Tails Won by: "$(($N-$M))
+      break
+   fi
+done
+
+   echo "Heads: "$N "Tails: "$M
+   if [ $N -eq $M ]
+   then
+		continue
+		if [ $(($N-$M)) -eq 2 ]
+		then
+		break
+   	fi
+	fi
